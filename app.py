5263d8ae1bdafafaf9bc26a1a93bb67386bf2efb
Farhan Syihab <m.farhanshihab11@gmail.com>
2022-06-17 00:15:21 +0700

    Added name.html, and modified navbar

--- app.py
+++ app.py
@@ -3,8 +3,15 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+#Flask Instance
 app = Flask(__name__, template_folder='templates')
+app.config['SECRET_KEY'] = "qwertyuiop"
 
+#Form Class
+class NameForm(FlaskForm):
+    name = StringField("Insert Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+    
 @app.route('/')
 def toHome():
     return redirect(url_for('home'))
@@ -20,6 +27,23 @@ def about():
 @app.route('/profile')
 def profile():
     return render_template("profile.html")
+
+
+@app.route('/name',methods=['GET','POST'])
+def name():
+    name = None
+    form = NameForm()
+    
+    #Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data  = ""
+        
+    return render_template("name.html",
+                           name = name,
+                           form = form
+                           )
+    
 
 if __name__ == "__main__":
     app.run(debug=True);
